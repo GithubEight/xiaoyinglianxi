fd0518f6810ce65da4c141128527b08dd2dfa375
xiaoying581 <2530401518@qq.com>
2021-01-27 01:16:49 +0800

    input和print函数的练习
    加深对input和print函数的认识

--- lianxi1.py
+++ lianxi1.py
@@ -1,2 +1,12 @@
 name=input("姓名")
 print("姓名",name,sep=':')
+
+
+name=input("请输入姓名:")
+qq=input("请输入qq:")
+phone=input("请输入电话:")
+school=input("请输入学校:")
+print("name",name,sep=':')
+print("qq",qq,sep=':')
+print("phone",phone,sep=':')
+print("school",school,sep=':')
